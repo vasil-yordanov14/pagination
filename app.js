60b3cd943378c6dd10856df0765a4e76d3c79e73
vasil-yordanov14 <vasil_sh@abv.bg>
2022-06-15 22:09:03 +0300

    btns setup

--- app.js
+++ app.js
@@ -4,12 +4,42 @@ import paginate from "./paginate.js";
 import displayButtons from "./displayButtons.js";
 
 const title = document.querySelector(`.section-title h1`);
+const btnContainer = document.querySelector(`.btn-container`);
+
+let index = 0;
+let pages = [];
+
+const setupUI = () => {
+  displayFollowers(pages[index]);
+  displayButtons(btnContainer, pages, index);
+};
 
 const init = async () => {
   const followers = await fetchFollowers();
-  displayFollowers(paginate(followers)[0]);
+
   title.textContent = `pagination`;
-  const pages = paginate(followers);
+  pages = paginate(followers);
+  setupUI();
 };
+
+btnContainer.addEventListener(`click`, function (e) {
+  if (e.target.classList.contains(`btn-container`)) return;
+  if (e.target.classList.contains(`page-btn`)) {
+    index = parseInt(e.target.dataset.index);
+  }
+  if (e.target.classList.contains(`next-btn`)) {
+    index++;
+    if (index > pages.length - 1) {
+      index = 0;
+    }
+  }
+  if (e.target.classList.contains(`prev-btn`)) {
+    index--;
+    if (index < 0) {
+      index = pages.length - 1;
+    }
+  }
+  setupUI();
+});
 
 window.addEventListener(`load`, init);
