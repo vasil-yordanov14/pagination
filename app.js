07e911e8e36ca86e1bd289025e8738a1972f940e
vasil-yordanov14 <vasil_sh@abv.bg>
2022-06-15 21:18:22 +0300

    create pages

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-import fetchFollowers from './fetchFollowers.js'
-import displayFollowers from './displayFollowers.js'
-import paginate from './paginate.js'
-import displayButtons from './displayButtons.js'
+import fetchFollowers from "./fetchFollowers.js";
+import displayFollowers from "./displayFollowers.js";
+import paginate from "./paginate.js";
+import displayButtons from "./displayButtons.js";
+
+const title = document.querySelector(`.section-title h1`);
+
+const init = async () => {
+  const followers = await fetchFollowers();
+  displayFollowers(paginate(followers)[0]);
+  title.textContent = `pagination`;
+  const pages = paginate(followers);
+};
+
+window.addEventListener(`load`, init);
--- displayFollowers.js
+++ displayFollowers.js
@@ -1,3 +1,18 @@
-const display = () => {}
+const container = document.querySelector(`.container`);
 
-export default display
+const display = (followers) => {
+  const newFollowers = followers
+    .map((person) => {
+      const { avatar_url, login, html_url } = person;
+      return `<article class="card">
+    <img src="${avatar_url}" alt="${login} />"
+    <h3>${login}</h3>
+    <div><a href="${html_url}" class="btn">view profile</a></div>
+    
+    </article>`;
+    })
+    .join(``);
+  container.innerHTML = newFollowers;
+};
+
+export default display;
